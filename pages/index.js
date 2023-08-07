091f94de723f0cc15e62a79f4f7350090a7ee6ba
WilliamEGD <williamegd@gmail.com>
2023-08-06 21:18:29 -0400

    added image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import styles from '../styles/Home.module.css';
-
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className={styles.center}>
       <h1>Devonshire Music</h1>
+
+      <Image
+        src='/music website main page.png'
+        width={300}
+        height={300}
+        alt="Picture Test"
+        className={styles.image}
+        suppressHydrationWarning={true}
+      />
     </div>
   )
 }
