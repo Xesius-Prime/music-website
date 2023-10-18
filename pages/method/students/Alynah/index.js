71d1a5fb94d986d861c8581a2c8b279e3ce87f63
WilliamEGD <williamegd@gmail.com>
2023-10-18 00:42:21 -0400

    added some stuff to Amanh and Alynahs pages

--- pages/method/students/Alynah/index.js
+++ pages/method/students/Alynah/index.js
@@ -5,38 +5,58 @@ function index() {
     return (
         <div>
             <h1>Alynah</h1>
-            <div className={globalStyle.divider}></div>
+            <br /><br />
 
-            <h2>General Overview</h2>
-            <div className={globalStyle.leftAlign}>
-                <h3>Note Recognition High Score:</h3>
-                <p>10/15 in 1min 16sec, set Oct 17 2023</p>
+            <div className={globalStyle.mainFocus}>
                 <br />
-
-                <h3>Book:</h3>
-                <p>Nothing yet. Likely we'll start with Piano Adventure Book 1</p>
+                <h2>Main Focus:</h2>
+                <div className={globalStyle.leftAlign}>
+                    <p>1. <a href="https://www.musictheory.net/exercises/note/oynsyryyynyyyry" className={globalStyle.highlightAndUnderline}>Note Recognition</a>.</p>
+                    <p>2. Nursery Rhyme songs, with note names.</p>
+                </div>
+                <br /><br />
             </div>
             <br />
 
-            <div className={globalStyle.divider}></div>
 
-            <h2>Oct 17 2023</h2>
 
-            <div className={globalStyle.leftAlign}>
-                <h3>Overview:</h3>
-                <p>We essentially worked on note recognition.</p>
-                <p>I will send her nursery rhymes with note names.</p>
-                <br />
+            <div className={globalStyle.goldBorder}>
+                <h2>Top Overview:</h2>
+                <div className={globalStyle.leftAlign}>
+                    <h3>Note Recognition High Score:</h3>
+                    <p>10/15 in 1min 16sec, set Oct 17 2023</p>
+                    <br />
 
-                <h3>Pieces:</h3>
-                <p>This was her first piano lesson with me.</p>
-                <br />
-
-                <h3>Misc:</h3>
-                <p></p>
+                    <h3>Book:</h3>
+                    <p>Nothing yet. We might start with Piano Adventure Book 1.</p>
+                </div>
+                <br /> <br />
             </div>
+            <br /> <br />
 
 
+
+            <div className={globalStyle.greenBorder}>
+
+                <h1>Individual Lessons</h1>
+
+                <div className={globalStyle.divider}></div>
+
+                <h2>Oct 17 2023</h2>
+
+                <h3>General</h3>
+                <div className={globalStyle.leftAlign}>
+                    <h3>Overview:</h3>
+                    <p>We essentially worked on note recognition.</p>
+                    <p>I will send her nursery rhymes with note names.</p>
+                    <br />
+
+                    <h3>Pieces:</h3>
+                    <p>This was her first piano lesson with me.</p>
+                    <br />
+                </div>
+
+            </div>
 
         </div>
     )
