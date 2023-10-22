4835c0f53492a4da4eb0a3c78e5d247b7de2b37e
WilliamEGD <williamegd@gmail.com>
2023-10-22 04:13:07 -0400

    updated Jennifers student page

--- pages/method/students/Jennifer/index.js
+++ pages/method/students/Jennifer/index.js
@@ -12,7 +12,7 @@ function index() {
                 <h2>Main Focus:</h2>
                 <div className={globalStyle.leftAlign}>
                     <p>1. Great Wall of China</p>
-                    <p>2. Improvisation in the Key of C# minor</p>
+                    <p>2. <a className={globalStyle.highlightAndUnderline} href="/method/piano/improvisation">Improvisation in the Key of C# minor</a></p>
                 </div>
                 <br /><br />
             </div>
@@ -42,9 +42,18 @@ function index() {
 
                 <div className={globalStyle.divider}></div>
 
+                <h2>Oct 21 2023</h2>
+
+                <h3></h3>
+                <div className={globalStyle.leftAlign}>
+                    <p>We mostly worked on playing the new song "Great Wall of China". Watch out for difference between quarter notes and eighths notes, (Ta's vs Ti Ti's). Make sure you aren't rushing the Ta's by turning them into Ti Ti's.</p>
+                    <p>We also did a brief refresher on improvisation in the key of C# minor.</p>
+                </div>
+                <br />
+
+                <div className={globalStyle.divider}></div>
+
                 <h2>Oct 14 2023</h2>
-
-
 
                 <h3>Improvisation:</h3>
                 <div className={globalStyle.leftAlign}>
